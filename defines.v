65bc37eea18c6e5cacb3fd7a21a31979ab8a95bc
xzjqx <xzjqx@outlook.com>
2016-07-01 15:13:54 +0800

    init

---
+++ defines.v
@@ -0,0 +1,24 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    15:05:45 07/01/2016 
+// Design Name: 
+// Module Name:    defines 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+
+`define RstEnable 1'b1
+`define RstDsiable 1'b0
+`define Rst
